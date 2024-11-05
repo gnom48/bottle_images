e086166ecd24c6582b024cf0474db36ee04766ea
Egor Ivanov <egorchima@gmail.com>
2024-11-05 11:40:28 +0300

    sg

--- app.py
+++ app.py
@@ -18,12 +18,4 @@ def index():
     '''
 
 if __name__ == '__main__':
-    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '6666'))
-    except ValueError:
-        PORT = 6666
-
-    run(app=app, host='localhost', port='6666')
+    run(app=app, host='localhost', port='8080')
